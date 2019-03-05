b6ef1bc2e3d09404d4682927b7904c716fce2ff9
JonathanFL <j.lydemann@gmail.com>
2019-03-05 11:03:39 +0100

    Everything is concurrent now, but it is slow. I can't figure out why. I use BackGroundWorker instead of Dispather.

---
+++ MultiThreading/CalcPi/CalculateDigitsOfPiInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcPi
+{
+    class CalculateDigitsOfPiInput
+    {
+        // USE A CLASS LIKE THIS, IF YOU WANT TO PASS MORE THAN ONE ARGUMENT THROUGH EVENT - fx from btnClick to DoWork
+        public int Digits { get; set; }
+
+        public CalculateDigitsOfPiInput(int digits)
+        {
+            Digits = digits;
+        }
+    }
+}
--- MultiThreading/CalcPi/MainWindow.xaml.cs
+++ MultiThreading/CalcPi/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,136 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CalcPi;
+using CalcPiAlgoritm;
 
-namespace CalcPi
+namespace SyncCalcPi
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int digits;
+        BackgroundWorker backgroundWorker;
+
+        DateTime startTime;
+        DateTime endTime;
+
         public MainWindow()
         {
             InitializeComponent();
+            backgroundWorker = ((BackgroundWorker)this.FindResource("BackgroundWorker"));
+            btnCancel.IsEnabled = false;
+        }
+        private void miFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            btnCalculate.IsEnabled = false;
+            btnCancel.IsEnabled = true;
+            // Update statusbar
+            sbiStatus.Content = "Calculating...";
+            progressBar.Visibility = Visibility.Visible;
+
+            // Convert control's decimal Value to an integer
+
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorker_OnDoWork);
+            backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(BackgroundWorker_OnProgressChanged);
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker_OnRunWorkerCompleted);
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+
+            // Convert control's decimal Value to an integer
+            //CalculateDigitsOfPiInput input = new CalculateDigitsOfPiInput(int.Parse(this.tbxDigits.Text));
+            digits = int.Parse(this.tbxDigits.Text);
+            //FindPrimesInput input = new FindPrimesInput(from, to);
+            startTime = DateTime.Now;
+            backgroundWorker.RunWorkerAsync(digits);
+
+            // Update statusbar
+            sbiStatus.Content = "Ready";
+        }
+        
+
+        private void BackgroundWorker_OnDoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            //Debug.WriteLine(input.Digits);
+
+            StringBuilder pi = new StringBuilder("3", digits + 2);
+
+            if (digits > 0)
+            {
+                pi.Append(".");
+
+                for (int i = 0; i < digits; i += 9)
+                {
+                    int nineDigits = NineDigitsOfPi.StartingAt(i + 1);
+                    int digitCount = Math.Min(digits - i, 9);
+                    string ds = $"{nineDigits:D9}";
+                    pi.Append(ds.Substring(0, digitCount));
+
+                    backgroundWorker.ReportProgress((i * 100) / digits, pi);
+                    if (backgroundWorker.CancellationPending)
+                    {
+                        break;
+                    }
+                }
+
+
+            }
+
+            if (backgroundWorker.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+        }
+
+        private void BackgroundWorker_OnProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            StringBuilder pi = e.UserState as StringBuilder;
+            tblkResults.Text = pi.ToString();// ?? throw new InvalidOperationException();
+            progressBar.Maximum = 100;
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Visibility = Visibility.Visible;
+            sbiStatus.Content = "Calculating";
+        }
+
+        private void BackgroundWorker_OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            endTime = DateTime.Now;
+            TimeSpan calcTime = endTime - startTime;
+            tblCalcTime.Text = calcTime.ToString();
+                // Reset UI
+            sbiStatus.Content = "Completed and Ready";
+            progressBar.Visibility = Visibility.Hidden;
+
+            btnCalculate.IsEnabled = true;
+            btnCancel.IsEnabled = false;
+
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Search cancelled.");
+                sbiStatus.Content = "Cancelled but ready";
+            }
+            else if (e.Error != null)
+            {
+                // An error was thrown by the DoWork event handler.
+                MessageBox.Show(e.Error.Message, "An Error Occurred");
+            }
+
+        }
+
+        private void BtnCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            backgroundWorker.CancelAsync();
         }
     }
 }
